9fc40b132ae616ea3367ee02b7d185db96b1fc88
hadi <hadii.tanoomand@gmail.com>
2023-04-11 04:36:56 +0430

    make cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -7,7 +7,20 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <>
+      <HStack>
+        <VStack>
+          <Box borderRadius='xl' bg="white" color='black'>
+            <Image borderRadius='xl' src={imageSrc}></Image>
+            <Heading m={4} size="md">{title}</Heading>
+            <Text m={4} color="gray">{description}</Text>
+            <Text m={4} color="black">See More  <FontAwesomeIcon icon={faArrowRight} size="1x" /></Text>
+          </Box>
+        </VStack>
+      </HStack>
+    </>
+  );
 };
 
 export default Card;
